23a2e845a9c1a37fe9c6eea9501a8e95f3a5639a
surnaturel <numirix.yannick@gmail.com>
2023-06-11 01:08:20 +0000

    mon commit du 11.06.2023

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-var formulaire_bancaire = document.getElementById('formulaire_bancaire')
-var valide = document.getElementById('valide')
+var valide = document.getElementById('valide');
+var listeMembre = [];
 
 valide.addEventListener("click", function() {
-    var nom = document.getElementById('nom').value
-    var prenom = document.getElementById('prenom').value
-    var code = document.getElementById('code').value
-    var code_validation = document.getElementById('code_validation').value
-    var afficher_code = document.getElementById('afficher_code').value
-    console.log(afficher_code)
-})
+    var listeActuel = {};
 
+    class insertionMembre {
+        constructor(code, code_validation, prenom, nom, solde) {
+            this.code = code;
+            this.code_validation = code_validation;
+            this.prenom = prenom;
+            this.nom = nom;
+            this.solde = solde;
+            this.formulaire = document.getElementById('formulaire_bancaire');
+            this.formulaire.addEventListener('submit', this.valider.bind(this));
+        }
 
+        valider(event) {
+            if (this.code !== this.code_validation) {
+                event.preventDefault();
+                console.log('Le mot de passe est incorrect.');
+                return;
+            }
+        }
+        ajouteMembre(liste){
+            liste.push({[nom] : this.nom})
+        }
+    }
+
+    var code = document.getElementById('code').value;
+    var code_validation = document.getElementById('code_validation').value;
+    var prenom = document.getElementById('prenom').value;
+    var nom = document.getElementById('nom').value;
+    var solde = 0;
+
+    var verification = new insertionMembre(code, code_validation, prenom, nom, solde);
+    verification.ajouteMembre(listeMembre)
+});
+
+var afficher_code = document.getElementById('afficher_code');
+var code_validation = document.getElementById('code_validation');
+
+afficher_code.addEventListener('change', function() {
+    if (afficher_code.checked) {
+        code_validation.setAttribute('type', 'text');
+    } else {
+        code_validation.setAttribute('type', 'password');
+    }
+});
