d6790434b6a2436f506aaf5a43b3f80ed8b521ce
surnaturel <numirix.yannick@gmail.com>
2023-10-15 12:02:03 +0000

    Débutez avec React

--- projet cours React/la-maison-jungle/src/components/Banner.js
+++ projet cours React/la-maison-jungle/src/components/Banner.js
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
 function Banner() {
-    const title = 'La maison jungle'
+    const title = 'La maison de edou jungle'
     return (
         <div className='lmj-banner'>
             <img src={logo} alt='La maison jungle' className='lmj-logo' />
